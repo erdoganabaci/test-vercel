29040b022147d2a616ae1cf94f1fb638064c5da4
erdoganabaci <erdoganabaci97@gmail.com>
2021-12-28 01:00:55 +0100

    verce test split file

--- api/index.js
+++ api/index.js
@@ -1,7 +1,8 @@
-import { ApolloServer, gql } from "apollo-server-express";
+import { ApolloServer } from "apollo-server-express";
 import { ApolloServerPluginDrainHttpServer } from "apollo-server-core";
 import http from "http";
 import express from "express";
+import typeDefs from "./typeDefs.js";
 
 const app = express();
 
@@ -9,19 +10,13 @@ app.use(express.json());
 
 const httpServer = http.createServer(app);
 
-const typeDefs = gql`
-  type Query {
-    hello: String
-  }
-`;
-
 const resolvers = {
   Query: {
     hello: () => "world",
   },
 };
 
-const startApolloServer = async(app, httpServer) => {
+const startApolloServer = async (app, httpServer) => {
   const server = new ApolloServer({
     typeDefs,
     resolvers,
@@ -30,7 +25,7 @@ const startApolloServer = async(app, httpServer) => {
 
   await server.start();
   server.applyMiddleware({ app });
-}
+};
 
 startApolloServer(app, httpServer);
 
